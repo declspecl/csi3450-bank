2482799f3eb8619c783ff777282979312ef5721d
declspecl <gavind2559@gmail.com>
2025-03-24 15:14:52 -0400

    Create get banks API route

--- bank-backend/src/queries/bank_queries.py
+++ bank-backend/src/queries/bank_queries.py
@@ -1,22 +1,22 @@
-def get_all_banks():
-    """
-    Retrieves all banks from the banks table.
-    """
-    query = "SELECT * FROM banks;"
-    return query
+from typing import Optional, TypedDict, Unpack
 
-def filter_banks_by_name(name_filter):
-    """
-    Retrieves banks whose names match the search term.
-    """
-    query = "SELECT * FROM banks WHERE name ILIKE %s"
-    params = (f"%{name_filter}%",) # Wrap the filter with % to allow partial matching.
-    return query, params
+class GetPartialMatchBanksQueryParams(TypedDict):
+    name: Optional[str]
+    location: Optional[str]
 
-def filter_banks_by_location(location_filter):
+def get_partial_match_banks_query(**kwargs: Unpack[GetPartialMatchBanksQueryParams]) -> tuple[str, tuple[str, str]]:
     """
-    Retrieves banks whose locations match with the search term.
+    Retrieves banks based on the provided filters.
+    If no filters are provided, retrieves all banks.
     """
-    query = "SELECT * FROM banks WHERE location ILIKE %s;"
-    params = (f"%{location_filter}%",)
+
+    name = kwargs.get("name")
+    location = kwargs.get("location")
+
+    query = "SELECT * FROM banks WHERE name ILIKE %s AND location ILIKE %s"
+    params: tuple[str, str] = (
+        "%{}%".format(name if name else ""),
+        "%{}".format(location if location else "")
+    )
+
     return query, params
