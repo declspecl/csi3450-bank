db4aaabdd8e9aa3c731a6636b47e9cdff082fd94
Austin Toma <austinttoma@gmail.com>
2025-03-24 11:20:40 -0400

    Added bank_queries and a test to run it, also added psycopg2 in requirements.txt for the sake of simplicity when testing

---
+++ bank-backend/src/test_bankqueries.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+from queries.bank_queries import (
+    get_all_banks,
+    filter_banks_by_name,
+    filter_banks_by_location
+)
+
+# Establish a connection to your PostgreSQL database
+conn = psycopg2.connect(
+    dbname="bank",
+    user="postgres",
+    password="postgres",
+    host="localhost"
+)
+cur = conn.cursor()
+
+# Test: Get all banks
+query = get_all_banks()
+cur.execute(query)
+banks = cur.fetchall()
+print("All Banks:", banks)
+
+# Test: Filter banks by name
+query, params = filter_banks_by_name("Chase")
+cur.execute(query, params)
+banks_by_name = cur.fetchall()
+print("Banks with 'Chase' in the name:", banks_by_name)
+
+# Test: Filter banks by location
+query, params = filter_banks_by_location("New York")
+cur.execute(query, params)
+banks_by_location = cur.fetchall()
+print("Banks located in New York:", banks_by_location)
+
+# Cleanup: Close the cursor and connection
+cur.close()
+conn.close()
